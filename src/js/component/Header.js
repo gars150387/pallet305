2404338be7283a9ec1400ba74699ad85c9fe5baf
gars150387 <gar.santeliz@gmail.com>
2022-03-26 20:28:26 +0000

    options added to component plus link to navigate thru components and icons linked to website

--- src/js/component/Header.js
+++ src/js/component/Header.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { FaFacebookF, FaInstagram } from 'react-icons/fa'
+import { ImWhatsapp } from 'react-icons/im'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+
+import '../../styles/header.css'
 
 export const Header = () =>{
     return (
@@ -9,7 +13,7 @@ export const Header = () =>{
             </div>
             <div className='row'>
                 <div className='col-10'></div>
-                <div className='col-2'><FaFacebookF/> <FaInstagram/> </div>
+                <div className='col-2'> <a href="https://www.facebook.com/Pallet305"><FaFacebookF/></a> <a href="https://www.instagram.com/pallet_305/"><FaInstagram/></a>  <ImWhatsapp /> </div>
             </div>
         </div>
     )
