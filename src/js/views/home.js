a2a40c289756a78859b11941993cc798cfe442e2
gars150387 <gar.santeliz@gmail.com>
2022-03-26 20:30:02 +0000

    carousel added

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,50 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import { Carousel } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../styles/home.css'
+// import "../../img/image1.JPG"
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+export const Home = () => {
+	return (
+		<div>
+			<Carousel>
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src="https://picsum.photos/200/300"
+						alt="First slide"
+					/>
+					{/* <Carousel.Caption>
+						<h3>First slide label</h3>
+						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					</Carousel.Caption> */}
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src="https://picsum.photos/200/300"
+						alt="Second slide"
+					/>
+
+					{/* <Carousel.Caption>
+						<h3>Second slide label</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</Carousel.Caption> */}
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src="https://picsum.photos/200/300"
+						alt="Third slide"
+					/>
+
+					{/* <Carousel.Caption>
+						<h3>Third slide label</h3>
+						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+					</Carousel.Caption> */}
+				</Carousel.Item>
+			</Carousel>
+		</div>
+	)
+};
