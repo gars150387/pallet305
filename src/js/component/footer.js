962e8c48e69d8b55059535d60df29f473395b585
gars150387 <gar.santeliz@gmail.com>
2022-03-26 22:23:28 +0000

    style imported

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import '../../styles/footer.css'
+import '../../styles/footer.css'
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
