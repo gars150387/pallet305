1714455e2e639978bc2a6e2e8e40a3dd3953e6c0
gars150387 <gar.santeliz@gmail.com>
2022-03-26 20:32:26 +0000

    component modified with different options and navegations

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
+import { Header } from "./component/Header";
+import { NavbarWebsite } from "./component/Navbar";
 import { Footer } from "./component/footer";
+import { About } from "./views/About";
+import { Design } from "./views/Design";
+import { Contact } from "./views/Contact";
+import { Policy } from "./views/Policy";
+import { Payment } from "./views/Payment";
 
 //create your first component
 const Layout = () => {
@@ -19,24 +22,32 @@ const Layout = () => {
 	return (
 		<div>
 			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
+					<Header />
+					<NavbarWebsite />
 					<Switch>
 						<Route exact path="/">
 							<Home />
 						</Route>
-						<Route exact path="/demo">
-							<Demo />
+						<Route exact path="/about">
+							<About />
 						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
+						<Route exact path="/design">
+							<Design />
+						</Route>
+						<Route exact path="/contact">
+							<Contact />
+						</Route>
+						<Route exact path="/policy">
+							<Policy />
+						</Route>
+						<Route exact path="/payment">
+							<Payment />
 						</Route>
 						<Route>
 							<h1>Not found!</h1>
 						</Route>
 					</Switch>
 					<Footer />
-				</ScrollToTop>
 			</BrowserRouter>
 		</div>
 	);
