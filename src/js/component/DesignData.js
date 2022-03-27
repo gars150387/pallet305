4bdffb3bdcda29b3d37489c59c2615234bcc0f33
gars150387 <gar.santeliz@gmail.com>
2022-03-27 22:55:52 +0000

    data structure being in progress

---
+++ src/js/component/DesignData.js
@@ -0,0 +1,76 @@
+export const DesignData = [{
+    'id': 01,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 02,
+    img: '/18A20FB2-818E-4B72-BE1E-BDECCC52134D[1].JPG'
+}, {
+    'id': 03,
+    img: '/29D529C0-9834-4926-A781-C6C881C0A2DE[1].JPG'
+}, {
+    'id': 04,
+    img: '/37d5499f-af1e-46ca-a14a-892a230d5aa0[1].JPG'
+}, {
+    'id': 05,
+    img: '/58E5E329-E64F-4972-9392-A6CEAC17A913.JPG'
+}, {
+    'id': 06,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 07,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 08,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 09,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 10,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 11,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 12,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 13,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 14,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 15,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 16,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 17,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 18,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 19,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 20,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 21,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 22,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 23,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 24,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}, {
+    'id': 25,
+    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+}]
