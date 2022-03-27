f3c0fc71c985a9fdd9888530d563bae7d7e65fab
gars150387 <gar.santeliz@gmail.com>
2022-03-27 15:53:10 +0000

    added layout grip from react bootstrap to become responsive component

--- src/js/component/Header.js
+++ src/js/component/Header.js
@@ -2,20 +2,32 @@ import React from 'react'
 import { FaFacebookF, FaInstagram } from 'react-icons/fa'
 import { ImWhatsapp } from 'react-icons/im'
 
+import { Container, Row, Col } from 'react-bootstrap'
+
 import '../img/logo.png'
 
 import '../../styles/header.css'
 
-export const Header = () =>{
+export const Header = () => {
     return (
-        <div>
-            <div className='row' id='div-logo'>
-                <img src='/logo.png' alt='logo' id='logo' />
-            </div>
-            <div className='row'>
-                <div className='col-10'></div>
-                <div className='col-2'> <a href="https://www.facebook.com/Pallet305"><FaFacebookF/></a> <a href="https://www.instagram.com/pallet_305/"><FaInstagram/></a>  <ImWhatsapp /> </div>
-            </div>
-        </div>
+        <Container>
+            <Row>
+                <Col><img src='/logo.png' alt='logo' id='logo' /></Col>
+                <Col xs={5}></Col>
+                <Col><a href="https://www.facebook.com/Pallet305"><FaFacebookF /></a> <a href="https://www.instagram.com/pallet_305/"><FaInstagram /></a>  <ImWhatsapp /></Col>
+            </Row>
+        </Container>
+
+
+
+        // <div>
+        //     <div className='row' id='div-logo'>
+
+        //     </div>
+        //     <div className='row'>
+        //         <div className='col-10'></div>
+        //         <div className='col-2'>  </div>
+        //     </div>
+        // </div>
     )
 }
