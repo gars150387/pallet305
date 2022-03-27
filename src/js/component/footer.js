771bd3256e5d498e12d8317bb4e09409fea263bb
gars150387 <gar.santeliz@gmail.com>
2022-03-27 16:39:32 +0000

    icons added with link

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,31 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { FaFacebookF, FaInstagram } from 'react-icons/fa'
+import { ImWhatsapp } from 'react-icons/im'
+
 import { Container, Row, Col } from "react-bootstrap";
 
 import '../../styles/footer.css'
 
 export const Footer = () => (
 	<div className="footer">
-			<Container className="sticky-bottom" bg="dark" variant="dark">
-		<Row md={4}>
-			<Col><Link to="./about">
-				<h5 >About</h5>
-			</Link>
-				<Link to="./design">
-					<h5>Design</h5>
+		<Container className="sticky-bottom" bg="dark" variant="dark">
+			<Row md={4}>
+				<Col><Link to="./about">
+					<h5 >About</h5>
 				</Link>
-				<Link to="./contact">
-					<h5>Contact</h5>
-				</Link></Col>
-			<Col xs={6}><Link to="./policy">
-				<h5>Policy</h5>
-			</Link>
-				<Link to="./payment">
-					<h5>Payment</h5>
-				</Link></Col>
-			<Col>3 of 3</Col>
-		</Row>
-	</Container>
+					<Link to="./design">
+						<h5>Design</h5>
+					</Link>
+					<Link to="./contact">
+						<h5>Contact</h5>
+					</Link></Col>
+				<Col xs={6}><Link to="./policy">
+					<h5>Policy</h5>
+				</Link>
+					<Link to="./payment">
+						<h5>Payment</h5>
+					</Link></Col>
+				<Col>
+					<a href="https://www.facebook.com/Pallet305"><FaFacebookF id='icons' /></a>
+					<a href="https://www.instagram.com/pallet_305/"><FaInstagram id='icons' /></a>
+					<a href="https://wa.me/message/SXDE7GX6G4OUF1"><ImWhatsapp id='icons' /></a>
+				</Col>
+			</Row>
+		</Container>
 	</div>
 );
