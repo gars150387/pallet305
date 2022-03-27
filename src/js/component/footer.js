5c9ff8f07d83f172cef3c89893f7fd649f2c4329
gars150387 <gar.santeliz@gmail.com>
2022-03-27 16:11:03 +0000

    using react bootstrap tlayout grip to become responsive component

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,29 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+import { Container, Row, Col } from "react-bootstrap";
+
 import '../../styles/footer.css'
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<div className="left">
-			<Link to="./about">
+	<div className="footer">
+			<Container className="sticky-bottom" bg="dark" variant="dark">
+		<Row md={4}>
+			<Col><Link to="./about">
 				<h5 >About</h5>
 			</Link>
-			<Link to="./design">
-				<h5>Design</h5>
-			</Link>
-			<Link to="./contact">
-				<h5>Contact</h5>
-			</Link>
-			<div className="right">
-				<Link to="./policy">
-					<h5>Policy</h5>
+				<Link to="./design">
+					<h5>Design</h5>
 				</Link>
+				<Link to="./contact">
+					<h5>Contact</h5>
+				</Link></Col>
+			<Col xs={6}><Link to="./policy">
+				<h5>Policy</h5>
+			</Link>
 				<Link to="./payment">
 					<h5>Payment</h5>
-				</Link>
-			</div>
-
-
-		</div>
-	</footer>
+				</Link></Col>
+			<Col>3 of 3</Col>
+		</Row>
+	</Container>
+	</div>
 );
