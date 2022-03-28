5f27532240316de8d6e650ec2610802766063026
gars150387 <gar.santeliz@gmail.com>
2022-03-28 22:49:56 +0000

    cleaning up data

--- src/js/component/DesignData.js
+++ src/js/component/DesignData.js
@@ -15,62 +15,62 @@ export const DesignData = [{
     img: '/58E5E329-E64F-4972-9392-A6CEAC17A913.JPG'
 }, {
     'id': 06,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 07,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 08,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 09,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 10,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 11,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 12,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 13,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 14,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 15,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 16,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 17,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 18,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 19,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 20,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 21,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 22,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 23,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 24,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }, {
     'id': 25,
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].JPG'
+    img: '/'
 }]
