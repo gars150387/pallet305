95172b673b6e8d15205596792cd8fd628197d24e
gars150387 <gar.santeliz@gmail.com>
2022-03-29 23:09:40 +0000

    component got into a navbar component and set fixed bottom feature

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -5,49 +5,55 @@ import { FaFacebookF, FaInstagram } from 'react-icons/fa'
 import { ImWhatsapp } from 'react-icons/im'
 import { CgCopyright } from 'react-icons/cg'
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Navbar, Container, Row, Col } from "react-bootstrap";
 
 import '../../styles/footer.css'
 
 export const Footer = () => (
 	<div className="footer">
-		<Container>
-			<Row md={4}>
-				<Row></Row>
-				<Col><Link to="./about">
-					<h5 >About</h5>
-				</Link>
-					<Link to="./design">
-						<h5>Design</h5>
+		<Navbar bg="dark" variant="dark" fixed="bottom">
+			<Container>
+				<Row md={4} >
+					<Row></Row>
+					<Col><Link to="./about">
+						<h5 >About</h5>
 					</Link>
-					<Link to="./contact">
-						<h5>Contact</h5>
-					</Link></Col>
-				<Col xs={6}><Link to="./policy">
-					<h5>Policy</h5>
-				</Link>
-					<Link to="./payment">
-						<h5>Payment</h5>
-					</Link></Col>
-				<Col>
-					<Row xs="auto">
-						<Col xs lg="2"><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Pallet305"><FaFacebookF id='iconsFooter' /></a></Col>
-						<Col xs lg="2"><a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pallet_305/"><FaInstagram id='iconsFooter' /></a></Col>
-						<Col xs lg="2"><a target="_blank" rel="noopener noreferrer" href="https://wa.me/message/SXDE7GX6G4OUF1"><ImWhatsapp id='iconsFooter' /></a></Col>
-					</Row>
-				</Col>
-			</Row>
-			<Row>
-				<Col></Col>
-				<Col xs={5}>
-					<Row className="justify-content-md-center">
-						<Col xs lg="2"></Col>
-						<Col md="auto"><p>Copyright <CgCopyright /> 2022 Pallet305 </p></Col>
-						<Col xs lg="2"></Col>
-					</Row>
-				</Col>
-				<Col></Col>
-			</Row>
-		</Container>
+						<Link to="./design">
+							<h5>Design</h5>
+						</Link>
+						<Link to="./contact">
+							<h5>Contact</h5>
+						</Link></Col>
+					<Col xs={6}><Link to="./policy">
+						<h5>Policy</h5>
+					</Link>
+						<Link to="./payment">
+							<h5>Payment</h5>
+						</Link></Col>
+					<Col>
+						<Row xs="auto">
+							<Col xs lg="2"><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Pallet305"><FaFacebookF id='iconsFooter' /></a></Col>
+							<Col xs lg="2"><a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pallet_305/"><FaInstagram id='iconsFooter' /></a></Col>
+							<Col xs lg="2"><a target="_blank" rel="noopener noreferrer" href="https://wa.me/message/SXDE7GX6G4OUF1"><ImWhatsapp id='iconsFooter' /></a></Col>
+						</Row>
+					</Col>
+				</Row>
+			</Container>
+			<Container>
+				<Row>
+					<Col></Col>
+					<Col xs={5}>
+						<Row className="justify-content-md-center">
+							<Col xs lg="2"></Col>
+							<Col md="auto"><p>Copyright <CgCopyright /> 2022 Pallet305 </p></Col>
+							<Col xs lg="2"></Col>
+						</Row>
+					</Col>
+					<Col></Col>
+				</Row>
+			</Container>
+		</Navbar>
+		{/* <Container fixed="bottom">
+		</Container> */}
 	</div>
 );
