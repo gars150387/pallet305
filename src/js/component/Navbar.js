1ee733200d996dd41cdae7a12997be79dafebdc1
gars150387 <gar.santeliz@gmail.com>
2022-03-30 00:03:52 +0000

    layout re adjusted

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Navbar, Container, Nav } from 'react-bootstrap'
+import { Row, Col, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 import { FaFacebookF, FaInstagram } from 'react-icons/fa'
 import { ImWhatsapp } from 'react-icons/im';
 
@@ -8,6 +8,8 @@ import '../img/officalLogo.jpg';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../styles/navbar.css'
+import { Link } from "react-router-dom";
 
 
 export const NavbarWebsite = () => {
@@ -16,43 +18,56 @@ export const NavbarWebsite = () => {
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
                 <Container>
                     <Row>
-                        <Col><img src='/officalLogo.jpg' alt='logo' id='logo' /></Col>
-                        <Col xs={5}></Col>
+
+                        <Col md="auto">
+                            <Link to="/">
+                                <img src='/officalLogo.jpg'
+                                    alt='logo'
+                                    id='logo'
+                                    width="10"
+                                    height="250"
+                                />
+                            </Link>
+                        </Col>
+                        <Col></Col>
                     </Row>
                 </Container>
                 <Container>
-                    <Navbar.Brand href="./"><h1>PALLET305</h1></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="navbarOptions">
-                            <Nav.Link href="./about">About</Nav.Link>
-                            <Nav.Link href="./design">Design</Nav.Link>
-                            <Nav.Link eventKey={2} href="./contact">
-                                Contact us
-                            </Nav.Link>
-                            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <Row>
+                        <Navbar.Brand href="./"><h1>PALLET305</h1></Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="navbarOptions">
+                                <Nav.Link href="./about">About</Nav.Link>
+                                <Nav.Link href="./design">Design</Nav.Link>
+                                <Nav.Link eventKey={2} href="./contact">
+                                    Contact us
+                                </Nav.Link>
+                                <NavDropdown title="" id="collasible-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">English</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/3.2">Spanish</NavDropdown.Item>
+                                    {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown> */}
-                        </Nav>
-                    </Navbar.Collapse>
-                    <Col>
-                        <Row className="justify-content-md-center">
-                            <Col xs lg="2">
-                                <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Pallet305"><FaFacebookF id='icons' /></a>
-                            </Col>
-                            <Col xs lg="2">
-                                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pallet_305/"><FaInstagram id='icons' /></a>
-                            </Col>
-                            <Col xs lg="2">
-                                <a target="_blank" rel="noopener noreferrer" href="https://wa.me/message/SXDE7GX6G4OUF1"><ImWhatsapp id='icons' /></a>
-                            </Col>
-                        </Row>
-                    </Col>
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+                                </NavDropdown>
 
+                            </Nav>
+                        </Navbar.Collapse>
+                        <Col>
+                            <Row className="justify-content-md-center">
+                                <Col xs lg="2">
+                                    <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Pallet305"><FaFacebookF id='icons' /></a>
+                                </Col>
+                                <Col xs lg="2">
+                                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pallet_305/"><FaInstagram id='icons' /></a>
+                                </Col>
+                                <Col xs lg="2">
+                                    <a target="_blank" rel="noopener noreferrer" href="https://wa.me/message/SXDE7GX6G4OUF1"><ImWhatsapp id='icons' /></a>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
                 </Container>
             </Navbar>
         </div>
