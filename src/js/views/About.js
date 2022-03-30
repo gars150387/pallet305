0edf62254a2b918c5e88cfa40add7d990ab0697d
gars150387 <gar.santeliz@gmail.com>
2022-03-30 00:04:25 +0000

    text added to push paragraph down

--- src/js/views/About.js
+++ src/js/views/About.js
@@ -5,6 +5,32 @@ export const About = () => {
     return (
         <div>
             <Container>
+                {/* esto es para hacer espacio y presionar los componentes hacia abajo */}
+                <Row xs={1} md={2}>
+                    <Col>z</Col>
+                    <Col>z</Col>
+                    <Col>z</Col>
+                </Row>
+                <Row xs={1} md={2}>
+                    <Col>z</Col>
+                    <Col>z</Col>
+                    <Col>z</Col>
+                </Row>
+                <Row xs={1} md={2}>
+                    <Col>z</Col>
+                    <Col>z</Col>
+                    <Col>z</Col>
+                </Row>
+                <Row xs={1} md={2}>
+                    <Col>z</Col>
+                    <Col>z</Col>
+                    <Col>z</Col>
+                </Row>
+                <Row xs={1} md={2}>
+                    <Col>z</Col>
+                    <Col>z</Col>
+                </Row>
+
                 <Row>
                     <Col>
                         <h3 id="about">
