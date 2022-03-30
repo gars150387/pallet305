a2e93b82d081053434fdb255565b5f06d08c2eff
gars150387 <gar.santeliz@gmail.com>
2022-03-30 00:03:19 +0000

    component commented

--- src/js/component/Header.js
+++ src/js/component/Header.js
@@ -1,19 +1,14 @@
-import React from 'react'
+// import React from 'react'
 
-import { Container, Row, Col } from 'react-bootstrap'
+// import { Container, Row, Col } from 'react-bootstrap'
 
-import '../img/logo.png'
-import '../img/officalLogo.jpg'
+// import '../img/logo.png'
+// import '../img/officalLogo.jpg'
 
-import '../../styles/header.css'
+// import '../../styles/header.css'
 
-export const Header = () => {
-    return (
-        <Container>
-            <Row>
-                <Col><img src='/officalLogo.jpg' alt='logo' id='logo' /></Col>
-                <Col xs={5}></Col>
-            </Row>
-        </Container>
-    )
-}
+// export const Header = () => {
+//     return (
+
+//     )
+// }
