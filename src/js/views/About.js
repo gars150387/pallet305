d149b1da23226e165c984c47f2e10bb6825a6abd
gars150387 <gar.santeliz@gmail.com>
2022-04-01 01:10:48 +0000

    set paragraph and image

--- src/js/views/About.js
+++ src/js/views/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 
+import '../../styles/about.css';
+
 import '../img/front.jpg'
 import '../img/85793fa7-72fb-492f-bc21-6a6def86bcf1.jpg';
 import '../img/f358a545-0748-4364-8c33-75d0764380f2.jpg'
@@ -10,20 +12,29 @@ export const About = () => {
         <div>
             <Container className='aboutContainer'>
                 <Row>
-                    <Col>
-                        <h3 id="about">
+                    <Col className='text-about'>
+                        <h4 >
                             Pallet305 is a company that was born in 2019, it specializes in creating, designing and modifying outdoor furniture, our product is exported from Spain, which is certified and treated.
-                        </h3>
+                        </h4>
+                        <br/>
+                        <h4 >
+                            Some of our most demanded jobs for patios are sofas, coffee tables, planters, bars, loungers, mailboxes and much more variety. For interiors, custom beds, sliding doors, etc.
+                        </h4>
+                        <br/>
+                        <h4 >
+                            We build pergolas and wooden floors (PT) to everyone's taste and satisfaction.
+                            Our priority is the customer, we are proud to be able to provide the best service and quality with our products.
+                        </h4>
                     </Col>
                     <Col>
                     <img
-								className="d-block w-100"
+								className="d-block w-100 m-4"
 								src={require("../img/front.jpg").default}
 								alt="" />
                     
                     </Col>
                 </Row>
-                <Row>
+                {/* <Row>
                     <Col>
                     <img
 								className="d-block w-100"
@@ -49,7 +60,7 @@ export const About = () => {
 								src={require("../img/f358a545-0748-4364-8c33-75d0764380f2.jpg").default}
 								alt="" />
                     </Col>
-                </Row>
+                </Row> */}
                 {/* <Row>
                     <Col>1 of 2</Col>
                     <Col>2 of 2</Col>
