60bdf07e7000be0f509528ec146318f967d042a8
gars150387 <gar.santeliz@gmail.com>
2022-04-02 18:48:31 +0000

    component to render img from designdata

---
+++ src/js/component/DesignTemplate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+export const DesignTemplate = ({img}) => {
+    return (
+        <Card className="bg-dark text-white">
+            <Card.Img src={ `../img/${ img }` } alt={ img } />
+            {/* <Card.ImgOverlay>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay> */}
+        </Card>
+    )
+}
