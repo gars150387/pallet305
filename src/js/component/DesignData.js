877aeef61c7ce4ab40a2fd27455c2b7577c44114
gars150387 <gar.santeliz@gmail.com>
2022-04-02 19:13:35 +0000

    data modified

--- src/js/component/DesignData.js
+++ src/js/component/DesignData.js
@@ -1,73 +1,73 @@
 export const DesignData = [{
-    img: '/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].jpg'
+    img: '../img/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85[1].jpg'
 }, {
-    img: '/18A20FB2-818E-4B72-BE1E-BDECCC52134D[1].jpg'
+    img: '../img/18A20FB2-818E-4B72-BE1E-BDECCC52134D[1].jpg'
 }, {
-    img: '/29D529C0-9834-4926-A781-C6C881C0A2DE[1].jpg'
+    img: '../img/29D529C0-9834-4926-A781-C6C881C0A2DE[1].jpg'
 }, {
   
-    img: '/37d5499f-af1e-46ca-a14a-892a230d5aa0[1].jpg'
+    img: '../img/37d5499f-af1e-46ca-a14a-892a230d5aa0[1].jpg'
 }, {
     
-    img: '/58E5E329-E64F-4972-9392-A6CEAC17A913.jpg'
+    img: '../img/58E5E329-E64F-4972-9392-A6CEAC17A913.jpg'
 }, {
     
-    img: '/66B913C7-B3DC-4FE5-93CC-D4AD67EB9379.jpg'
+    img: '../img/66B913C7-B3DC-4FE5-93CC-D4AD67EB9379.jpg'
 }, {
     
-    img: '/69AFD7B7-FA23-4A54-AB51-7234A6795F2E.jpg'
+    img: '../img/69AFD7B7-FA23-4A54-AB51-7234A6795F2E.jpg'
 }, {
     
-    img: '/142B2076-642E-4451-8A54-F00F6822324A.jpg'
+    img: '../img/142B2076-642E-4451-8A54-F00F6822324A.jpg'
 }, {
     
-    img: '/567F3275-1455-4384-ABCC-53263531A3E1.jpg'
+    img: '../img/567F3275-1455-4384-ABCC-53263531A3E1.jpg'
 }, {
      
-    img: '/85793fa7-72fb-492f-bc21-6a6def86bcf1.jpg'
+    img: '../img/85793fa7-72fb-492f-bc21-6a6def86bcf1.jpg'
 }, {
      
-    img: '/2562834a-fd18-45d3-b519-9edc954a3acd.jpg'
+    img: '../img/2562834a-fd18-45d3-b519-9edc954a3acd.jpg'
 }, {
      
-    img: '/61556755-B9B9-4DB0-8162-B38A736A693D.jpg'
+    img: '../img/61556755-B9B9-4DB0-8162-B38A736A693D.jpg'
 }, {
      
-    img: '/A9927117-9F9A-44C2-9BEF-35876E24C7DF.jpg'
+    img: '../img/A9927117-9F9A-44C2-9BEF-35876E24C7DF.jpg'
 }, {
      
-    img: '/E2F7BDFE-FE72-48E0-B12F-441DEFEA0DC9.jpg'
+    img: '../img/E2F7BDFE-FE72-48E0-B12F-441DEFEA0DC9.jpg'
 }, {
      
-    img: '/ED549812-A8F7-4D45-8697-116BA1FBB1F1.jpg'
+    img: '../img/ED549812-A8F7-4D45-8697-116BA1FBB1F1.jpg'
 }, {
     
-    img: '/f358a545-0748-4364-8c33-75d0764380f2.jpg'
+    img: '../img/f358a545-0748-4364-8c33-75d0764380f2.jpg'
 }, {
     
-    img: '/FE859BF3-BE14-4E3F-A83C-5D4163248E01.jpg'
+    img: '../img/FE859BF3-BE14-4E3F-A83C-5D4163248E01.jpg'
 }, {
      
-    img: '/img:_4199.jpg'
+    img: '../img/img_4199.jpg'
 }, {
      
-    img: '/img:_6388.jpg'
+    img: '../img/img_6388.jpg'
 }, {
      
-    img: '/img:_6389.jpg'
+    img: '../img/img_6389.jpg'
 }, {
      
-    img: '/img:_7267.jpg'
+    img: '../img/img_7267.jpg'
 }, {
      
-    img: '/img:_7808.jpg'
+    img: '../img/img_7808.jpg'
 }, {
      
-    img: '/img:_8029.jpg'
+    img: '../img/img_8029.jpg'
 }, {
      
-    img: '/img:_8424.jpg'
+    img: '../img/img_8424.jpg'
 }, {
      
-    img: '/img:_8585.jpg'
+    img: '../img/img_8585.jpg'
 }]
