b6f099da1fc2d0de6cbfd7198ccceb7635689357
gars150387 <gar.santeliz@gmail.com>
2022-04-05 00:05:43 +0000

    img link changed

--- src/js/component/DesignTemplate.js
+++ src/js/component/DesignTemplate.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 
 import "../../styles/design.css";
 
-export const DesignTemplate = ({img}) => {
+export const DesignTemplate = ({ img }) => {
     return (
-        <Card className="bg-dark text-white">
-            <Card.Img src={ `../img/${ img }` } alt={ img } id="designImg" />
-            {/* <Card.ImgOverlay>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                </Card.Text>
-                <Card.Text>Last updated 3 mins ago</Card.Text>
-            </Card.ImgOverlay> */}
-        </Card>
+        <Col sm >
+            <Card className="bg-dark text-white">
+                <Row className="row no-gutters">
+                    <Col xs lg={8}>
+                        <Card.Img src={`../../img/${img}.jpg`} alt={img} id="designImg" />
+                    </Col>
+                </Row>
+            </Card>
+        </Col>
+
     )
 }
