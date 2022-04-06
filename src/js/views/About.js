2af85f91745f6a4c9f06a3fdbc2991a43f6ef505
gars150387 <gar.santeliz@gmail.com>
2022-04-06 00:01:06 +0000

    magazine image added

--- src/js/views/About.js
+++ src/js/views/About.js
@@ -4,8 +4,8 @@ import { Container, Row, Col } from 'react-bootstrap';
 import '../../styles/about.css';
 
 import '../img/front.jpg'
-import '../img/85793fa7-72fb-492f-bc21-6a6def86bcf1.jpg';
-import '../img/f358a545-0748-4364-8c33-75d0764380f2.jpg'
+import '../img/PHOTO-2022-03-24-15-34-13.jpg';
+import '../img/PHOTO-2022-03-24-15-34-37.jpg'
 
 export const About = () => {
     return (
@@ -16,59 +16,29 @@ export const About = () => {
                         <h4 >
                             Pallet305 is a company that was born in 2019, it specializes in creating, designing and modifying outdoor furniture, our product is exported from Spain, which is certified and treated.
                         </h4>
-                        <br/>
+                        <br />
                         <h4 >
                             Some of our most demanded jobs for patios are sofas, coffee tables, planters, bars, loungers, mailboxes and much more variety. For interiors, custom beds, sliding doors, etc.
                         </h4>
-                        <br/>
+                        <br />
                         <h4 >
                             We build pergolas and wooden floors (PT) to everyone's taste and satisfaction.
                             Our priority is the customer, we are proud to be able to provide the best service and quality with our products.
                         </h4>
                     </Col>
                     <Col>
-                    <img
-								className="d-block w-100 m-4"
-								src={require("../img/front.jpg").default}
-								alt="" />
-                    
+                        <img
+                            className="d-block w-100 m-4"
+                            src={require("../img/front.jpg").default}
+                            alt="" />
+
                     </Col>
                 </Row>
-                {/* <Row>
-                    <Col>
-                    <img
-								className="d-block w-100"
-								src={require("../img/85793fa7-72fb-492f-bc21-6a6def86bcf1.jpg").default}
-								alt="" />
-                    </Col>
-                    <Col>
-                        <h3 id="about">
-                            Some of our most demanded jobs for patios are sofas, coffee tables, planters, bars, loungers, mailboxes and much more variety. For interiors, custom beds, sliding doors, etc.
-                        </h3>
-                    </Col>
-                </Row>
+                <hr />
                 <Row>
-                    <Col>
-                        <h3 id="about">
-                            We build pergolas and wooden floors (PT) to everyone's taste and satisfaction.
-                            Our priority is the customer, we are proud to be able to provide the best service and quality with our products.
-                        </h3>
-                    </Col>
-                    <Col>
-                    <img
-								className="d-block w-100"
-								src={require("../img/f358a545-0748-4364-8c33-75d0764380f2.jpg").default}
-								alt="" />
-                    </Col>
-                </Row> */}
-                {/* <Row>
-                    <Col>1 of 2</Col>
-                    <Col>2 of 2</Col>
+                    <Col className='m-2'><img src={require('../img/PHOTO-2022-03-24-15-34-13.jpg').default } alt='PHOTO-2022-03-24-15-34-13' /></Col>
+                    <Col ><img src='PHOTO-2022-03-24-15-34-37.jpg' alt='PHOTO-2022-03-24-15-34-37' /></Col>
                 </Row>
-                <Row>
-                    <Col>1 of 2</Col>
-                    <Col>2 of 2</Col>
-                </Row> */}
             </Container>
         </div>
     )
