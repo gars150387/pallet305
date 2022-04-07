a54436a1a202f2f22a928c249c1595dd448c96e8
gars150387 <gar.santeliz@gmail.com>
2022-04-07 23:45:30 +0000

    layout and styling

--- src/js/component/DesignTemplate.js
+++ src/js/component/DesignTemplate.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Card, Col } from 'react-bootstrap';
 
 import "../../styles/design.css";
 
 export const DesignTemplate = ({ img }) => {
     return (
-        <Col xs lg={5} >
-            <Card className="bg-dark text-white">
-                <Col>
-                    <Card.Img src={`${img}.jpg`} alt={img} id="designImg" />
+        <Col xs lg={ 8 } className="m-2 justify-content-center" id="template">
+            <Card className="bg-dark text-white" id="template">
+                <Col></Col>
+                <Col xs={6} className="m-1 justify-content-center" id="template">
+                    <Card.Img className="justify-content-center" src={`${img}.jpg`} alt={img} id="designImg" />
                 </Col>
+                <Col></Col>
             </Card>
         </Col>
-
     )
 }
