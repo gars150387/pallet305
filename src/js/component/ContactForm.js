aaddf701ec4cd3c33992b8da0b329eefad65e947
gars150387 <gar.santeliz@gmail.com>
2022-04-07 00:02:52 +0000

    assigning onwer data

--- src/js/component/ContactForm.js
+++ src/js/component/ContactForm.js
@@ -7,19 +7,19 @@ export const ContactForm = () => {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_tqq2iw2', 'template_l0cnn0y', e.target, 'user_7hiY7EgOGmmB04VGtCfcX')
+        emailjs.sendForm('service_6pkt6sa', 'template_1m1i12l', e.target, '-mMBXm7P-f0DthEpF')
             .then((result) => {
-                console.log(result.text);
+                console.log(result.text)
             }, (error) => {
-                console.log(error.text);
+                throw (error)
             });
-            e.target.reset()
+        e.target.reset()
     };
 
     return (
         <div className='container'>
-            <form 
-            onSubmit={sendEmail}
+            <form
+                onSubmit={sendEmail}
             >
                 <div className='="row pt-5 mx-auto'>
                     <div className='col-10 form-group mx-auto'>
@@ -35,7 +35,7 @@ export const ContactForm = () => {
                         <textarea type="text" className='form-control' id='' cols='30' rows="8" placeholder='Your Message' name='message' />
                     </div>
                     <div className='col-10 pt-3 mx-auto mb-4' >
-                        <input type="submit" className='btn btn-info' placeholder='Send Message' />
+                        <input type="submit" className='btn btn-success' placeholder='Send Message' />
                     </div>
                 </div>
             </form>
