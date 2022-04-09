857c53a3390c80729cbe5d6ca2e9045cf6d00bae
gars150387 <gar.santeliz@gmail.com>
2022-04-09 21:39:22 +0000

    component to place each carousel in a organzied way

---
+++ src/js/component/CarouselItem4.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Carousel, Row, Col, Container } from "react-bootstrap";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../styles/home.css';
+
+import '../img/FE859BF3-BE14-4E3F-A83C-5D4163248E01.jpg'
+import '../img/142B2076-642E-4451-8A54-F00F6822324A.jpg'
+import '../img/567F3275-1455-4384-ABCC-53263531A3E1.jpg'
+
+export const CarouselItem4 = () => {
+	return (
+		<div>
+			<Container fluid>
+				<Row>
+					<Col >
+						<Carousel fade>
+							<Carousel.Item>
+								<Row>
+									<img
+										className="d-block w-100"
+										src={require("../img/FE859BF3-BE14-4E3F-A83C-5D4163248E01.jpg").default}
+										alt="" />
+								</Row>
+							</Carousel.Item>
+							<Carousel.Item>
+								<Row>
+									<img
+										className="d-block w-100"
+										src={require("../img/142B2076-642E-4451-8A54-F00F6822324A.jpg").default}
+										alt="" />
+								</Row>
+							</Carousel.Item>
+							<Carousel.Item>
+								<Row>
+									<img
+										className="d-block w-100"
+										src={require("../img/567F3275-1455-4384-ABCC-53263531A3E1.jpg").default}
+										alt="" />
+
+								</Row>
+							</Carousel.Item>
+						</Carousel>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	)
+};
