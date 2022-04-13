197ef3c58ff1412921463c70f0a30dd47bb1b48d
gars150387 <gar.santeliz@gmail.com>
2022-04-13 00:03:37 +0000

    readjusting layout to become a responsive app in mobile screen

--- src/js/component/DesignTemplate.js
+++ src/js/component/DesignTemplate.js
@@ -5,16 +5,11 @@ import "../../styles/design.css";
 
 export const DesignTemplate = ({ img }) => {
     return (
-        <Col xs lg={ 8 } className="m-2 justify-content-center animate__animated animate__fadeInDownBig" id="template">
-            <Card className="bg-dark text-white" id="template">
-                <Col></Col>
-                <Col xs={6} className="m-1 justify-content-center animate__animated animate__fadeInRightBig" id="template">
-                    <Card.Img className="justify-content-center" src={`${img}.jpg`} alt={img} id="designImg" />
-                </Col>
-                <Col></Col>
-            </Card>
+        <Col xs lg={10} className="m-2 justify-content-center animate__animated animate__fadeInDownBig" id="template">
+            <Col className="m-1 justify-content-center animate__animated animate__fadeInRightBig" id="template">
+                <Card.Img className="justify-content-center" src={`${img}.jpg`} alt={img} id="designImg" />
+            </Col>
         </Col>
     )
 }
 
-// className="animate__animated animate__fadeInUp" 
