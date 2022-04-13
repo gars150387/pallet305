c7a6e8e801744ec50c59fa447facf6e26316f493
gars150387 <gar.santeliz@gmail.com>
2022-04-13 01:42:00 +0000

    changed layout size

--- src/js/component/DesignTemplate.js
+++ src/js/component/DesignTemplate.js
@@ -5,7 +5,7 @@ import "../../styles/design.css";
 
 export const DesignTemplate = ({ img }) => {
     return (
-        <Col xs lg={10} className="m-2 justify-content-center animate__animated animate__fadeInDownBig" id="template">
+        <Col xs lg={12} className="m-2 justify-content-center animate__animated animate__fadeInDownBig" id="template">
             <Col className="m-1 justify-content-center animate__animated animate__fadeInRightBig" id="template">
                 <Card.Img className="justify-content-center" src={`${img}.jpg`} alt={img} id="designImg" />
             </Col>
